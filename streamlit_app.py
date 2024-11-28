80c1d96c116226a0b9668572b689956dc8f2495b
userness <auth.microsoft.no.reply@gmail.com>
2024-11-28 16:19:09 -0500

    Update streamlit_app.py
    Added page links

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ hello = open("hello.md", "r")
 hello = hello.read()
 
 st.markdown(hello)
+
+st.divider()
+
+st.link_button("Go to downloads", "pages/downloads.py")
+st.link_button("Go to the generator", "pages/gen.py")
