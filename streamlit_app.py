e309e3f3595749f4bd3203dd4d999d27a7faeabd
userness <auth.microsoft.no.reply@gmail.com>
2024-11-28 15:12:58 -0500

    Update streamlit_app.py
    Changed from defaults

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+hello = open("hello.md", "r")
+hello = hello.read()
+
+st.markdown(hello)
