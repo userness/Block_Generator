f93f1c1de51057e116f806e2723f2e83417b3083
userness <auth.microsoft.no.reply@gmail.com>
2024-11-28 20:05:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ col1, col2 = st.columns(2)
 
 # Add buttons in separate columns to place them side by side
 with col1:
-    st.link_button("Go to downloads", "pages/Downloads.py")
+    st.page_link("pages/Downloads.py", label="To Downloads", icon="⬇️")
 with col2:
-    st.link_button("Go to the generator", "pages/Generator.py")
+    st.page_link("pages/Generator.py", label="To The Generator", icon="⚙️")
